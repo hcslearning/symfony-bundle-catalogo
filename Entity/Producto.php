42eb66ea97b2b714a7cd629e25be1a01554d07d6
HCS Learning <learning@hardcybersoft.cl>
2019-09-28 02:39:33 -0300

    Bundle Catalogo init

---
+++ Entity/Producto.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace HCS\CatalogoBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity
+ */
+class Producto
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $nombre;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $precio;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $imagen;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $descripcion;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getNombre(): ?string
+    {
+        return $this->nombre;
+    }
+
+    public function setNombre(string $nombre): self
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+
+    public function getPrecio(): ?float
+    {
+        return $this->precio;
+    }
+
+    public function setPrecio(float $precio): self
+    {
+        $this->precio = $precio;
+
+        return $this;
+    }
+
+    public function getImagen(): ?string
+    {
+        return $this->imagen;
+    }
+
+    public function setImagen(string $imagen): self
+    {
+        $this->imagen = $imagen;
+
+        return $this;
+    }
+
+    public function getDescripcion(): ?string
+    {
+        return $this->descripcion;
+    }
+
+    public function setDescripcion(string $descripcion): self
+    {
+        $this->descripcion = $descripcion;
+
+        return $this;
+    }
+}
---
+++ HCSCatalogoBundle.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace HCS\CatalogoBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Doctrine\Bundle\DoctrineBundle\DependencyInjection\Compiler\DoctrineOrmMappingsPass;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+
+class HCSCatalogoBundle extends Bundle {
+
+
+}
